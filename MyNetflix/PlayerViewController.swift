6e41d91a213538e179941b9c7d02a0adc376f6ce
yeongwoo <jyy0223@naver.com>
2021-05-01 12:59:11 +0900

    ADD Player Logic in PlayerViewController

--- MyNetflix/PlayerViewController.swift
+++ MyNetflix/PlayerViewController.swift
@@ -6,18 +6,63 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayerViewController: UIViewController {
 
+    @IBOutlet weak var playerView: PlayerView!
+    @IBOutlet weak var controlView: UIView!
+    @IBOutlet weak var playButton: UIButton!
+    
+    let player = AVPlayer()
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .landscapeRight
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        playerView.player = player
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.play()
+    }
+    
+    @IBAction func togglePlayButton(_ sender: Any) {
+        if player.isPlaying {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
+    func play() {
+        player.play()
+        playButton.isSelected = true
+    }
+    func pause() {
+        player.pause()
+        playButton.isSelected = false
+    }
+    
+    func reset() {
+        pause()
+        player.replaceCurrentItem(with: nil)
     }
     
     @IBAction func close(_ sender: Any) {
+        reset()
         dismiss(animated: false, completion: nil)
+    }
+}
+
+extension AVPlayer {
+    var isPlaying: Bool {
+        guard self.currentItem != nil else {
+            return false
+        }
+        return self.rate != 0
     }
 }
