9f7ddf559a53d61c8979a364ba5cf169d13898b1
yeongwoo <jyy0223@naver.com>
2021-05-01 12:56:38 +0900

    ADD PlayerViewLayer

---
+++ MyNetflix/PlayerView.swift
@@ -0,0 +1,28 @@
+//
+//  PlayerView.swift
+//  MyNetflix
+//
+//  Created by 조영우 on 2021/04/30.
+//
+import UIKit
+import AVFoundation
+
+class PlayerView: UIView {
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    // Override UIView property
+    override static var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+}
